bc0a7df0ea1570bb249ecf7498b245eb9cb1b844
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-09-10 13:05:47 +0530

    X01BD: Update overlays
    * Remove unsupported hidl hals
    
    Change-Id: I0e3a2cc814ca502dc8caa782d32814f7647e0d52

--- device.mk
+++ device.mk
@@ -40,9 +40,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
-
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -277,8 +275,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # HIDL
 PRODUCT_PACKAGES += \
